9d23c0ceaf6bec938df7e967da15e528142e8c1a
Anthony <grant.anthony@gmail.com>
2017-01-14 00:34:59 -0500

    Added index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = 'views/';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
 });
 
 module.exports = router;
